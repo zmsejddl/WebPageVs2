4982e1476d94f5541e012aeb527c283fabab529a
Jung Young Hun <hujung0268@gamil.com>
2024-07-10 17:55:27 +0900

    CRUD 리포지터리 만들기

--- cardatabase/src/main/java/com/mysite/cardatabase/domain/CarRepository.java
+++ cardatabase/src/main/java/com/mysite/cardatabase/domain/CarRepository.java
@@ -1,7 +1,35 @@
 package com.mysite.cardatabase.domain;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
 
+	//브랜드로 자동차를 검색
+	List<Car> findByBrand(String brand);
+	
+	//색상으로 자동차를 검색
+	List<Car> findByColor(String color);
+	
+	//연도로 자동차를 검색
+	List<Car> findByYear(int year);
+	
+	//브랜드와 모델로 자동차를 검색
+	List<Car> findByBrandAndModel(String brand, String model);
+	
+	//브랜드나 색상으로 자동차를 검색
+	List<Car> findByBrandOrColor(String brand, String color);
+	
+	//브랜드로 자동차를 검색하고 연도로 정렬
+	List<Car> findByBrandOrderByYearAsc(String brand);
+	
+//	//SQL 문을 이용해 브랜드로 자동차를 검색
+//	@Query("select c from Car c where c.brand = ?1")
+//	List<Car> findByBrand(String brand);
+	
+	//SQL 문을 이용해 브랜드로 자동차를 검색
+	@Query("select c from Car c where c.brand like %?1")
+	List<Car> findByBrandEndsWith(String brand);
 }
